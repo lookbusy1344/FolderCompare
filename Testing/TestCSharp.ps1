f27597b373e1a6ca9e92b45c10de953369246c27
John <john@john.com>
2023-07-11 10:57:38 +0100

    Improved the test scripts

--- Testing/TestCSharp.ps1
+++ Testing/TestCSharp.ps1
@@ -1,4 +1,4 @@
-function displayresults([string]$title, [object[]]$results) {
+function Show-Results([string]$title, [object[]]$results) {
     Write-Host
     if ($null -eq $results) {
         Write-Host "$title"
@@ -11,33 +11,16 @@ function displayresults([string]$title, [object[]]$results) {
     }
 }
 
-
-Write-Host "Testing CSharp Folder Compare"
-$expected1 = Get-Content -Path "./csharpresults_name.txt" | Sort-Object
-$actual1 = foldercompare.exe -a ./foldera -b ./folderb -c name -r | Sort-Object
-$results1 = Compare-Object $expected1 $actual1
-
-$expected2 = Get-Content -Path "./csharpresults_namesize.txt" | Sort-Object
-$actual2 = foldercompare.exe -a ./foldera -b ./folderb -c namesize -r | Sort-Object
-$results2 = Compare-Object $expected2 $actual2
-
-$expected3 = Get-Content -Path "./csharpresults_hash.txt" | Sort-Object
-$actual3 = foldercompare.exe -a ./foldera -b ./folderb -c hash -r | Sort-Object
-$results3 = Compare-Object $expected3 $actual3
-
-displayresults "Compare by name" $results1
-displayresults "Compare by name+size" $results2
-displayresults "Compare by hash" $results3
-
-
-<# function Compare-Folders([string]$comparisonType) {
+function Compare-Folders([string]$comparisonType) {
     $expected = Get-Content -Path "./csharpresults_$comparisonType.txt" | Sort-Object
     $actual = foldercompare.exe -a ./foldera -b ./folderb -c $comparisonType -r | Sort-Object
     $results = Compare-Object $expected $actual
-    displayresults "Compare by $comparisonType" $results
+    Show-Results "Compare by $comparisonType" $results
 }
+
+
+Write-Host "Testing CSharp Folder Compare"
 
 Compare-Folders "name"
 Compare-Folders "namesize"
 Compare-Folders "hash"
- #>
--- Testing/TestRust.ps1
+++ Testing/TestRust.ps1
@@ -1,4 +1,4 @@
-function displayresults([string]$title, [object[]]$results) {
+function Show-Results([string]$title, [object[]]$results) {
     Write-Host
     if ($null -eq $results) {
         Write-Host "$title"
@@ -11,20 +11,15 @@ function displayresults([string]$title, [object[]]$results) {
     }
 }
 
+function Compare-Folders([string]$comparisonType) {
+    $expected = Get-Content -Path "./rustresults_$comparisonType.txt" | Sort-Object
+    $actual = folder_compare.exe -a ./foldera -b ./folderb -c $comparisonType -r | Sort-Object
+    $results = Compare-Object $expected $actual
+    Show-Results "Compare by $comparisonType" $results
+}
 
 Write-Host "Testing Rust Folder Compare"
-$expected1 = Get-Content -Path "./rustresults_name.txt" | Sort-Object
-$actual1 = folder_compare.exe -a ./foldera -b ./folderb -c name -r | Sort-Object
-$results1 = Compare-Object $expected1 $actual1
 
-$expected2 = Get-Content -Path "./rustresults_namesize.txt" | Sort-Object
-$actual2 = folder_compare.exe -a ./foldera -b ./folderb -c namesize -r | Sort-Object
-$results2 = Compare-Object $expected2 $actual2
-
-$expected3 = Get-Content -Path "./rustresults_hash.txt" | Sort-Object
-$actual3 = folder_compare.exe -a ./foldera -b ./folderb -c hash -r | Sort-Object
-$results3 = Compare-Object $expected3 $actual3
-
-displayresults "Compare by name" $results1
-displayresults "Compare by name+size" $results2
-displayresults "Compare by hash" $results3
+Compare-Folders "name"
+Compare-Folders "namesize"
+Compare-Folders "hash"
