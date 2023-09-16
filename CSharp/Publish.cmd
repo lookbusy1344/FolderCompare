d75783c276c6e55d065b52a238b83f3ea4f43632
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-09-16 09:52:03 +0100

    Switching the C# app to native AOT compilation

--- CSharp/Publish.cmd
+++ CSharp/Publish.cmd
@@ -1,7 +1,7 @@
 @echo off
 echo Cleaning...
 dotnet clean
-echo Publishing single file...
-dotnet publish -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true
-cd .\bin\Release\net7.0\win-x64\publish\
+echo Publishing native binary...
+rem dotnet publish -c Release -r win-x64 --self-contained false /p:PublishSingleFile=true
+dotnet publish FolderCompare.csproj -r win-x64 -c Release
 pause
