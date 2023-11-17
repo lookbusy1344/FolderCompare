7d044e76f697d01942144e2936aba914b415d5fb
lookbusy1344 <3680611+lookbusy1344@users.noreply.github.com>
2023-11-17 12:54:06 +0000

    Inline array now uses bytes instead of ulongs

--- CSharp/Hashes.cs
+++ CSharp/Hashes.cs
@@ -7,78 +7,88 @@ namespace FolderCompare;
 #pragma warning disable IDE1006 // Upper case first letter for public members
 
 /// <summary>
-/// A struct containing an inline array of 4 ulongs, large enough for a SHA2 hash
+/// A struct containing an inline array of 32 bytes, large enough for a SHA2 hash
 /// </summary>
 #pragma warning disable CA1815, IDE0051, IDE0044    // warnings related to inline arrays, nothing significant                                                             
-[System.Runtime.CompilerServices.InlineArray(4)]
+[System.Runtime.CompilerServices.InlineArray(Sha2Value.Size)]
 internal struct InnerSha2
 {
-	private ulong _element0;
+	private byte _element0;
 }
 #pragma warning restore CA1815, IDE0051, IDE0044
 
 /// <summary>
 /// A struct record holding the SHA-2 hash of a file. This is a value type for speed
-/// 4 ulongs are 32 bytes in total
 /// </summary>
-[System.Diagnostics.DebuggerDisplay("{val[0]}-{val[1]}-{val[2]}-{val[3]}")]
+[System.Diagnostics.DebuggerDisplay("{this.ToString()}")]
 public readonly record struct Sha2Value
 {
 	/// <summary>
-	/// Empty value: 0, 0, 0, 0
+	/// Empty value: 32 x 0 bytes
 	/// </summary>
 	public static readonly Sha2Value Empty;
 
 	/// <summary>
-	/// The inline array of 4 ulongs
+	/// Size in bytes of the hash
+	/// </summary>
+	public const int Size = 32;
+
+	/// <summary>
+	/// The inline array of 32 bytes
 	/// </summary>
 	private readonly InnerSha2 val;
 
 	/// <summary>
 	/// Inline arrays lack automatic value semantics (like normal arrays), so we need to implement them manually
 	/// </summary>
-	public readonly bool Equals(Sha2Value other) => val[0] == other.val[0] && val[1] == other.val[1] && val[2] == other.val[2] && val[3] == other.val[3];
-
-	/// <summary>
-	/// For speed, just use the first ulong as the hash code
-	/// </summary>
-	public override readonly int GetHashCode() => val[0].GetHashCode();
-
-	/// <summary>
-	/// Construct SHA2 value from 4 ulongs
-	/// </summary>
-	public Sha2Value(ulong a, ulong b, ulong c, ulong d)
+	public readonly bool Equals(Sha2Value other)
 	{
-		val[0] = a;
-		val[1] = b;
-		val[2] = c;
-		val[3] = d;
+		for (var i = 0; i < Size; i++)
+			if (val[i] != other.val[i])
+				return false;
+
+		return true;
 	}
+
+	public override readonly int GetHashCode() => HashCode.Combine(val[0], val[2], val[10], val[12], val[20], val[22], val[31]);
 
 	/// <summary>
 	/// construct SHA2 value from a byte span
 	/// </summary>
 	public Sha2Value(ReadOnlySpan<byte> bytes)
 	{
-		if (bytes.Length != 32)
+		if (bytes.Length != Size)
 			throw new ArgumentException("The byte span must contain exactly 32 bytes.", nameof(bytes));
 
-		val[0] = BitConverter.ToUInt64(bytes[..8]);
-		val[1] = BitConverter.ToUInt64(bytes.Slice(8, 8));
-		val[2] = BitConverter.ToUInt64(bytes.Slice(16, 8));
-		val[3] = BitConverter.ToUInt64(bytes.Slice(24, 8));
+		bytes.CopyTo(val);
 	}
+
+#if DEBUG
+	/// <summary>
+	/// Just a diagnostic constructor to create a Sha2Value from 4 ulong values
+	/// </summary>
+	public Sha2Value(ulong a, ulong b, ulong c, ulong d)
+	{
+		Span<byte> bytes = stackalloc byte[Size];
+
+		HashUtils.WriteBytes(bytes, a);
+		HashUtils.WriteBytes(bytes.Slice(8, 8), b);
+		HashUtils.WriteBytes(bytes.Slice(16, 8), c);
+		HashUtils.WriteBytes(bytes.Slice(24, 8), d);
+
+		bytes.CopyTo(val);
+	}
+#endif
 
 	/// <summary>
 	/// Turn the Sha2Value into a byte array
 	/// </summary>
 	public byte[] ToBytes()
 	{
-		var bytes = new byte[32];
-		HashUtils.WriteBytes(bytes.AsSpan(), val[0]);
-		HashUtils.WriteBytes(bytes.AsSpan(sizeof(ulong)), val[1]);
-		HashUtils.WriteBytes(bytes.AsSpan(2 * sizeof(ulong)), val[2]);
-		HashUtils.WriteBytes(bytes.AsSpan(3 * sizeof(ulong)), val[3]);
+		var bytes = new byte[Size];
+
+		ReadOnlySpan<byte> src = val;
+		src.CopyTo(bytes);
 
 		return bytes;
 	}
@@ -88,19 +98,17 @@ public readonly record struct Sha2Value
 	/// </summary>
 	public void ToBytes(Span<byte> bytes)
 	{
-		//if (bytes.Length != 32)
+		//if (bytes.Length != Size)
 		//	throw new ArgumentException("The byte span must contain exactly 32 bytes.", nameof(bytes));
 
-		HashUtils.WriteBytes(bytes, val[0]);
-		HashUtils.WriteBytes(bytes[sizeof(ulong)..], val[1]);
-		HashUtils.WriteBytes(bytes[(2 * sizeof(ulong))..], val[2]);
-		HashUtils.WriteBytes(bytes[(3 * sizeof(ulong))..], val[3]);
+		ReadOnlySpan<byte> src = val;
+		src.CopyTo(bytes);
 	}
 
 	public override string ToString()
 	{
-		Span<byte> buff = stackalloc byte[32];
-		var builder = new StringBuilder(64);
+		Span<byte> buff = stackalloc byte[Size];
+		var builder = new StringBuilder(Size * 2);
 
 		ToBytes(buff);
 		foreach (var b in buff)
